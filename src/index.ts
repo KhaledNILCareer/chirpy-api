6ddac1cb4b15d98eba2de6145e2d5666e24e942a
KhaledNILCareer <KhaledNILCareer@gmail.com>
2026-09-23 11:37:37 +0300

    feat: add static file server and response logging middleware

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,48 @@
+import express from "express";
+import type {
+  Request,
+  Response,
+  NextFunction,
+} from "express";
+
+const app = express();
+const PORT = 8080;
+
+app.use(middlewareLogResponses);
+// Readiness endpoint
+app.get("/healthz", handlerReadiness);
+
+// Serve static files
+
+app.use("/app", express.static("./src/app"));
+
+function handlerReadiness(req: Request, res: Response): void {
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.send("OK");
+}
+
+ export function middlewareLogResponses(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void {
+  res.on("finish", () => {
+    const resCode = res.statusCode;
+
+    if (resCode < 200 || resCode > 299) {
+      console.log(
+        `[NON-OK] ${req.method} ${req.url} - Status: ${resCode}`
+      );
+    }
+  });
+
+  next();
+}
 
 
-console.log("Hello, World!")
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+
