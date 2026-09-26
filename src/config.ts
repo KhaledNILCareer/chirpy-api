eb77f46d9302fb504ff8328876c9f3dffff72a05
KhaledNILCareer <KhaledNILCareer@gmail.com>
2026-09-26 15:51:11 +0300

    feat: configure Drizzle client and add user creation query

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,22 @@
+import "dotenv/config";
+
+function envOrThrow(key: string): string {
+  const value = process.env[key];
+
+  if (!value) {
+    throw new Error(`Missing environment variable: ${key}`);
+  }
+
+  return value;
+}
+
 export type APIConfig = {
   fileserverHits: number;
+  dbURL: string;
 };
 
 export const config: APIConfig = {
   fileserverHits: 0,
+  dbURL: envOrThrow("DATABASE_URL"),
+  
 };
